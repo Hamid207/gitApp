04b7ec7d2b6b73cfa1a82ee66b5256030a6a0427
Hamid <hamidmanafov@gmail.com>
2020-11-21 01:10:39 +0400

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Manafov Hamid on 11/21/20.
+//  Copyright © 2020 Manafov Hamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
